fd1c2795ec5d55d2dfa21e95373eba1530614f11
John Nooney <1803534@uad.ac.uk>
2022-04-02 16:12:28 +0100

    Update half of template

---
+++ ProjectSource/CompilerStarter/Compiler/mypalcompiler/MyPALParser.cpp
@@ -0,0 +1,149 @@
+#include "MyPALParser.hpp"
+
+// Based off the program EBNF
+// <Program> ::= PROGRAM Identifier
+/*				WITH <VarDecls>
+				IN(<Statement>) +
+				END;
+*/
+void MyPALParser::recStarter() {
+	scanner().lex();
+
+	// start parsing
+	expect("PROGRAM");
+	expect(Token::Identifier);
+	expect("WITH");
+	recVarDecls();
+	expect("IN");
+	do {
+		recStatement();
+	} while (statementCheck());
+	expect("END");
+	expect(";");
+}
+
+bool MyPALParser::statementCheck() {
+	return have(Token::Identifier) || have("UNTIL") || have("IF") || have("INPUT") ? true : false;
+}
+
+// Based off the EBNF
+// <VarDecls> ::= (<IdentList> AS <Type>)* ;
+void MyPALParser::recVarDecls() {
+	while(have(Token::Identifier)) {
+		recIdentList();
+		expect("AS");
+
+		// TODO: refactor to use sema to register token
+		if (match("REAL") || match("INTEGER"))
+		{
+		} 
+		else {
+			// TODO: throw error missing type
+		}
+	}
+}
+
+// Based off the EBNF 
+// <Statement> ::= <Assignment> | <Loop> | <Conditional> | <I-o> ;
+void MyPALParser::recStatement()
+{
+	if (have(Token::Identifier)) {
+		// recognise Assignment
+	}
+	else if (have("UNTIL")) {
+		// recognise Loop
+	}
+	else if (have("IF")) {
+		// recognise Conditional
+	}
+	else if (have("INPUT")) {
+		// recognise IO
+	}
+	else {
+		// Throw error
+	}
+}
+
+// Based off the EBNF 
+// <Assignment> ::= Identifier = <Expression> ;
+void MyPALParser::recAssignment()
+{
+	expect(Token::Identifier);
+	expect("=");
+	recExpression();
+}
+
+// Based off the EBNF
+// <Conditional> ::= IF <BooleanExpr> THEN (<Statement>)*
+//						(ELSE(<Statement>)*)?
+//						ENDIF;
+void MyPALParser::recConditional()
+{
+	expect("IF");
+	recBooleanExpr();
+	expect("THEN");
+	// TODO: Make sure to only run this code when bool is true
+	while (statementCheck()) {
+		recStatement();
+	}
+	if (match("ELSE")) {
+		while (statementCheck()) {
+			recStatement();
+		}
+	}
+	expect("ENDIF");
+	expect(";");
+}
+
+// Based off the EBNF - returns bool based on the result
+// <BooleanExpr> ::= <Expression> ("<" | "=" | ">") <Expression> ;
+bool MyPALParser::recBooleanExpr()
+{
+	recExpression();
+
+	if (match("<")) {
+		// TODO: store operator for a less than comparison
+	}
+	else if (match(">")) {
+		// TODO: store operator for a greater than comparison
+	}
+	else if (match("=")) {
+		// TODO: store operator for an equal comparison
+	}
+	else {
+		// TODO: Throw Error
+	}
+
+	recExpression();
+
+	// TODO: perform comparison
+	return true;
+}
+
+// Based off the EBNF
+// <I-o> ::= INPUT <IdentList> | OUTPUT <Expression>(, <Expression>)*;
+void MyPALParser::recIO()
+{
+}
+
+// Based off the EBNF - should return the type
+// <Expression> ::= <Term> ( (+|-) <Term>)* ;
+void MyPALParser::recExpression()
+{
+}
+
+//void MyPALParser::recTerm()
+//{
+//}
+//
+//void MyPALParser::recFactor()
+//{
+//}
+//
+//void MyPALParser::recValue()
+//{
+//}
+//
+//void MyPALParser::recIdentList()
+//{
+//}
